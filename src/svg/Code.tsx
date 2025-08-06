d9cf735e71ebdbdbc9cbf8f8b37bf55fc16436bc
jinhengpang <jpan0070@student.monash.edu>
2025-08-06 17:17:01 +1000

    Resize header link svgs

--- src/svg/Code.tsx
+++ src/svg/Code.tsx
@@ -3,8 +3,8 @@ import type { SVGProps } from "react";
 const SvgCode = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="2em"
+    height="2em"
     fill="none"
     viewBox="0 0 24 24"
     {...props}
