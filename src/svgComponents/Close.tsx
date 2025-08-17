87daf87e81066fdc1ab39bb1729643df688b3bde
jinhengpang <pangjinheng58@gmail.com>
2025-08-17 14:15:23 +1000

    Import close & paper plane svg - create respective .tsx files

---
+++ src/svgComponents/Close.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgClose = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    stroke="#000"
+    strokeWidth={0}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#0F1729"
+      fillRule="evenodd"
+      stroke="none"
+      d="M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707a1 1 0 0 1 0-1.414"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgClose;
---
+++ src/svgComponents/PaperPlane.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgPaperPlane = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    id="paper-plane_svg__Icons"
+    viewBox="0 0 32 32"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <style>
+      {
+        ".paper-plane_svg__st0{fill:none;stroke:#000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10}"
+      }
+    </style>
+    <path
+      d="M26.4 2.9 3.8 15c-.7.4-.7 1.5.1 1.8l20.8 8.7c.6.3 1.3-.2 1.4-.8l1.7-20.8c.1-.9-.7-1.4-1.4-1"
+      className="paper-plane_svg__st0"
+    />
+    <path
+      d="M26 4 13 20v7.3c0 .9 1.2 1.4 1.8.7l4.2-5"
+      className="paper-plane_svg__st0"
+    />
+  </svg>
+);
+export default SvgPaperPlane;
