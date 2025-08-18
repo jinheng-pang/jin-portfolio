d28b5ead68acfce14f37e482f5c150d22b817bae
jinhengpang <pangjinheng58@gmail.com>
2025-08-18 20:23:06 +1000

    Isolate types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface ContactFormProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export interface ExperienceProps {
+    jobTitle: string;
+    company: string;
+    duration: string;
+    description: string;
+}
