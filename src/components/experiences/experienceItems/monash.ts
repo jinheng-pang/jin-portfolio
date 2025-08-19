7051eece3abd8fe2b0884d622697d278f1412b9e
jinhengpang <pangjinheng58@gmail.com>
2025-08-19 19:08:55 +1000

    Change naming convention for experience-items to experienceItems

--- src/components/experiences/experienceItems/monash.ts
+++ src/components/experiences/experienceItems/monash.ts
@@ -1,10 +1,10 @@
 import type { ExperienceProps } from "../../../types";
 
-const monash: ExperienceProps = {
+const Monash: ExperienceProps = {
     jobTitle: "Bachelor of Computer Science",
     company: "Monash University",
     duration: "Mar 2022 - Nov 2026",
     description: "Founded Monash which is now hosting 1 billion students worldwide"
 }
 
-export default monash;
+export default Monash;
