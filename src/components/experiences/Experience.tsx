4bec09cf6fb2851097868b463cbbf3067b8bd3a9
jinhengpang <pangjinheng58@gmail.com>
2025-08-19 21:48:33 +1000

    Style duration into two rows

--- src/components/experiences/Experience.tsx
+++ src/components/experiences/Experience.tsx
@@ -3,31 +3,33 @@ import MedHealth from "./experienceItems/medhealth";
 import Monash from "./experienceItems/monash";
 import ExperienceCard from "./experienceCards/ExperienceCard";
 
-import "./experience.css"
+import "./experience.css";
 
 const experiences: ExperienceProps[] = [MedHealth, Monash];
 
-
 const Experience = () => {
   return (
-    <div className="experience">
-        <h2 className="heading">Experience</h2>
-        <div className="timeline">
-            {experiences.map((exp, idx) => (
-                <li className="timelineItem" key={idx}>
-                    <time className="timelineDuration">{exp.duration}</time>
-                    <div className="timelineMiddle">
-                        <div className="timelinePoint">
-                        </div>
-                    </div>
-                    <div className="timelineContent">
-                        <ExperienceCard {...exp} />
-                    </div>
-                </li>
-            ))}
-        </div>
+    <div className="experience panel">
+      <h2 className="heading">Experience</h2>
+      <div className="timeline">
+        {experiences.map((exp, idx) => (
+          <li className="timelineItem" key={idx}>
+            <time className="timelineDuration">
+              {" "}
+              <span className="start">{exp.start} -</span>
+              <span className="end">{exp.end}</span>
+            </time>
+            <div className="timelineMiddle">
+              <div className="timelinePoint"></div>
+            </div>
+            <div className="timelineContent">
+              <ExperienceCard {...exp} />
+            </div>
+          </li>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
