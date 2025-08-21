654f0ac7e3686e246bbc31af984051c555b145ee
jinhengpang <pangjinheng58@gmail.com>
2025-08-21 20:47:16 +1000

    Import goal and folder svg - create .tsxt format

---
+++ src/svgComponents/Goal.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgGoal = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    className="goal_svg__icon goal_svg__line-color"
+    data-name="Line Color"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="m15 9-2.5 2.5M15 6v3h3l3-3h-3V3Z"
+      style={{
+        fill: "none",
+        stroke: "#2ca9bc",
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeWidth: 2,
+      }}
+    />
+    <path
+      d="M12.33 3H12a9 9 0 1 0 9 9v-.33"
+      style={{
+        fill: "none",
+        stroke: "#000",
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeWidth: 2,
+      }}
+    />
+    <path
+      d="M16.9 13A5 5 0 1 1 11 7.1"
+      data-name="primary"
+      style={{
+        fill: "none",
+        stroke: "#000",
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeWidth: 2,
+      }}
+    />
+  </svg>
+);
+export default SvgGoal;
