02cd732d3c3b93c18ba5520afb1b05f93e56c79b
jinhengpang <pangjinheng58@gmail.com>
2025-09-03 08:22:09 +1000

    Types files missing

---
+++ backend/src/types.ts
@@ -0,0 +1,14 @@
+export interface ValidationResult<T> {
+  error: Error | undefined;
+  value: T;
+}
+
+export type Token = string | null;
+
+export interface FormDataProps {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+  recaptchaToken: Token;
+}
