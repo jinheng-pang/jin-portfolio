4822dc1c11d42e6061c619a6e27a0ebf19a05c10
jinhengpang <pangjinheng58@gmail.com>
2025-09-03 07:53:16 +1000

    Brokte the code base due to configs and restructuring - all working now

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,32 +1,38 @@
-import type { ValidationResult, FormDataProps, Token } from "../frontend/types";
+import type {
+  ValidationResult,
+  FormDataProps,
+  Token,
+  RecaptchaResponse,
+} from "../../types.js";
 import type { Request, Response } from "express";
 
-require("dotenv").config(); // load environment variables (secrets)
-const express = require("express");
-const Joi = require("joi"); // validation library
-const cors = require("cors"); // allow API request
+import dotenv from "dotenv";
+import express from "express";
+import Joi from "joi"; // validation library
+import cors from "cors"; // allow API request
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config({ path: "./.env" });
 
 // Populate req body
 app.use(express.json());
 app.use(cors());
 
-export async function verifyRecaptcha(token: Token) {
+async function verifyRecaptcha(token: Token) {
   const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY;
   const recaptchaURL = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${token}`;
 
   const response = await fetch(recaptchaURL, { method: "POST" });
-  const result = await response.json();
+  const result = (await response.json()) as RecaptchaResponse;
 
   return result.success;
 }
 
-app.POST("/send-email", async (req: Request, res: Response) => {
+app.post("/send-email", async (req: Request, res: Response) => {
   const schema = Joi.object({
     name: Joi.string().min(5).required(),
-    email: Joi.string().min(5).email({ minDOmainSegments: 2 }).required(),
+    email: Joi.string().min(5).email({ minDomainSegments: 2 }).required(),
     subject: Joi.string().min(5).required(),
     message: Joi.string().min(5).required(),
     recaptchaToken: Joi.string().required(),
@@ -44,4 +50,6 @@ app.POST("/send-email", async (req: Request, res: Response) => {
     res.status(404).send("reCaptcha verification failed. Please try again!");
 });
 
-app.listen(PORT, () => console.log(`CORS-enabled server running on port ${PORT}...`));
+app.listen(PORT, () =>
+  console.log(`CORS-enabled server running on port ${PORT}...`)
+);
